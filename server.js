460ac5896280ec865bd60df2f39bd1f756e71bdd
Tony DeFreitas <anthony.defreitas@gmail.com>
2015-04-01 10:15:23 -0400

    init commit nba

---
+++ server.js
@@ -0,0 +1,24 @@
+var application_root = __dirname,
+    express          = require('express'),
+    bodyParser       = require('body-parser'),
+    path             = require('path'),
+    logger           = require('morgan'),
+    models           = require('./models');
+
+var app = express();
+
+// Server Configuration
+app.use(logger('dev'));
+app.use( bodyParser() );
+app.use( express.static( path.join( application_root, 'public' )))
+app.use( express.static( path.join( application_root, 'browser' )))
+
+// Routes
+
+app.listen(3000, function() {
+  console.log('....servers serve....');
+});
+
+
+// Export app as module
+module.exports = app;
